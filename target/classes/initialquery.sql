46289b6f7d8d54f64de6ef041affdcd7183ebba7
Soumya-Pattanshetti <119292362+Soumya-Pattanshetti@users.noreply.github.com>
2023-01-18 21:32:31 +0530

    zip file

---
+++ target/classes/initialquery.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user_db`.`role` (`id`, `name`) VALUES ('1', 'ROLE_GUEST');
+INSERT INTO `user_db`.`role` (`id`, `name`) VALUES ('2', 'ROLE_READER');
+INSERT INTO `user_db`.`role` (`id`, `name`) VALUES ('3', 'ROLE_AUTHOR');
